0dc1c86127170268f4b29e9886e99dda49a35689
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-16 19:13:10 +0000

    arraymap

--- arraymap.c
+++ arraymap.c
@@ -5,6 +5,19 @@
 #include "stdio.h"
 
 int main(){
+    int numbers[10];
 
+    /*populate the array*/
+    numbers[0]=10;
+    numbers[1]=20;
+    numbers[2]=30;
+    numbers[3]=40;
+    numbers[4]=50;
+    numbers[5]=60;
+    numbers[6]=70;
+
+    /* Print the 7th number from the array, which has an index of 6 */
+    printf("The 7th number in the array is %d", numbers[6]);
+    return 0;
 
 }
