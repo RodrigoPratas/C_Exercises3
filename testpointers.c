8d235e918d8168d695e834e5349b42b2bdf6953b
RodrigoPratas <rodrigompratas2002@gmail.com>
2023-02-17 22:21:22 +0000

    testpointer

--- testpointers.c
+++ testpointers.c
@@ -4,14 +4,25 @@
 // Just test for pointers!!!
 
 
-#include "stdio.h"
+#include <stdio.h>
+int main()
+{
+    int* pc, c;
 
-int main(){
-    int * pc, c;
-    c = 5;
+    c = 22;
+    printf("Address of c: %p\n", &c);
+    printf("Value of c: %d\n\n", c);  // 22
+
     pc = &c;
-    c = 1;
-    printf("%d \n", c);
-    printf("%d", *pc);
+    printf("Address of pointer pc: %p\n", pc);
+    printf("Content of pointer pc: %d\n\n", *pc); // 22
+
+    c = 11;
+    printf("Address of pointer pc: %p\n", pc);
+    printf("Content of pointer pc: %d\n\n", *pc); // 11
+
+    *pc = 2;
+    printf("Address of c: %p\n", &c);
+    printf("Value of c: %d\n\n", c); // 2
     return 0;
 }
